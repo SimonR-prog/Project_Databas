92b76707190097457483b007cd515f38bc470f10
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-02-05 10:07:39 +0100

    Adding route

--- Project_Databas/src/App.jsx
+++ Project_Databas/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import CreateProjects from './views/CreateProjects';
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/rojects' element={<Projects />} />
+          <Route path='/create' element={<CreateProjects />} />
+
+          <Route path='/' element={<CreateProjects />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
